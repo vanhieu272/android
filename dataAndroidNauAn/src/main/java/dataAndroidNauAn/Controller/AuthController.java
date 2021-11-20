20c389e28809b156373bec020e6c4b97f5fe6b81
vandongphamtk2001 <vandongphamtk282001@gmail.com>
2021-11-20 10:45:21 +0700

    commit

--- dataAndroidNauAn/src/main/java/dataAndroidNauAn/Controller/AuthController.java
+++ dataAndroidNauAn/src/main/java/dataAndroidNauAn/Controller/AuthController.java
@@ -1,23 +1,16 @@
 package dataAndroidNauAn.Controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import dataAndroidNauAn.dto.UserDTO;
-import dataAndroidNauAn.service.impl.UserService;
 
 @RestController
 public class AuthController {
 
-    @Autowired
-    private UserService userService;
-
-    @PostMapping(value="/register")
-    public UserDTO register(@RequestBody UserDTO user){
-        user.setPassWord(new BCryptPasswordEncoder().encode(user.getPassWord()));
-        return userService.save(user);
-    }
+//    @Autowired
+//    private UserService userService;
+//
+//    @PostMapping(value="/register")
+//    public UserDTO register(@RequestBody UserDTO user){
+//        user.setPassWord(new BCryptPasswordEncoder().encode(user.getPassWord()));
+//        return userService.save(user);
+//    }
 }
